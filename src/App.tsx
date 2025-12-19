f8667682a0f4887efbf9260211cbfeba2a47779b
Roman Imankulov <roman.imankulov@6sense.com>
2025-12-19 12:07:07 +0100

    Remove Nylas Scheduler H1

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import './App.css'
 function App() {
   return (
     <div className="app-container">
-      <h1>Nylas Scheduler</h1>
       <NylasScheduling configurationId="1d208806-40d9-4d40-a5bf-f39a54ed94f6" />
     </div>
   )
